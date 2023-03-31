f26c3df10074b85d3c59c88789319af3e5de6f54
berthold43 <thommy.johansson@sydmail.se>
2023-03-31 08:47:18 +0200

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,7 @@ module.exports = {
     title: `gatsby-begin`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-sass"]
+  plugins: ["gatsby-plugin-sass",
+                "gatsby-plugin-image",
+    "gatsby-plugin-sharp",]
 };
